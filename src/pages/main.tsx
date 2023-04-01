578657be2ce7bf5bf300a09fc1ade2be18e29eb3
NdipEstaban <estabannd@gmail.com>
2023-04-01 22:22:42 +0100

    built the app

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -63,9 +63,9 @@ const Main:React.FC<mainProps> = ({userDetails}):JSX.Element => {
       await setCurrentChat(userId);
     }
 
-    useEffect(() => {
-      let language = navigator.
-    }, []);
+    // useEffect(() => {
+    //   let language = navigator.
+    // }, []);
 
     const sendMessage = async(userId:string, messageData:messageItem) => {
       await addMessage(userId, messageData);
